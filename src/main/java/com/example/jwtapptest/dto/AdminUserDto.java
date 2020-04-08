93ab9ba32ba72e5a8c3bb61de99535630904a232
sofflen <nixxesgaming@gmail.com>
2020-04-08 17:17:25 +0300

    Added DTO layer

---
+++ src/main/java/com/example/jwtapptest/dto/AdminUserDto.java
@@ -0,0 +1,47 @@
+package com.example.jwtapptest.dto;
+
+import com.example.jwtapptest.model.Role;
+import com.example.jwtapptest.model.Status;
+import com.example.jwtapptest.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AdminUserDto {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String status;
+    private List<Role> roles;
+
+    public User toUser() {
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setStatus(Status.valueOf(status));
+        user.setRoles(roles);
+
+        return user;
+    }
+
+    public static AdminUserDto fromUser (User user) {
+        AdminUserDto adminUserDto = new AdminUserDto();
+        adminUserDto.setId(user.getId());
+        adminUserDto.setUsername(user.getUsername());
+        adminUserDto.setFirstName(user.getFirstName());
+        adminUserDto.setLastName(user.getLastName());
+        adminUserDto.setEmail(user.getEmail());
+        adminUserDto.setStatus(user.getStatus().name());
+        adminUserDto.setRoles(user.getRoles());
+
+        return adminUserDto;
+    }
+}
---
+++ src/main/java/com/example/jwtapptest/dto/RegisterDto.java
@@ -0,0 +1,12 @@
+package com.example.jwtapptest.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
